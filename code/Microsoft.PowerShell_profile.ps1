80270e7da914f06af52df2cae9b8a917a76b9900
Stephen Lingham <STEPHEN559@bailliegifford.com>
2022-09-12 10:04:12 +0100

    Added powershell profile

---
+++ code/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,69 @@
+Function EditProfile {
+    code $profile
+}
+
+set-alias ep EditProfile
+
+Function ReloadProfile {
+    . $profile
+}
+
+Function OpenCurrentDirectoryWithVsCode {
+    code .
+}
+
+set-alias c OpenCurrentDirectoryWithVsCode
+
+Function OpenCurrentDirectoryWithFileExplorer {
+    explorer .
+}
+
+set-alias e OpenCurrentDirectoryWithFileExplorer
+
+set-alias p pwd
+
+set-alias l ls
+
+Function U {
+    cd ..
+}
+
+Function Uu {
+    cd ../..
+}
+
+Function Uuu {
+    cd ../../..
+}
+
+Function Uuuu {
+    cd ../../../..
+}
+
+Function Uuuuu {
+    cd ../../../../..
+}
+
+Function GitStatus {
+    git status
+}
+
+set-alias gs GitStatus
+
+Function GitStageAll {
+    git stage .
+}
+
+set-alias gsa GitStageAll
+
+Function GitCommit($message) {
+    git commit -m $message
+}
+
+set-alias gcom GitCommit
+
+Function GitBranchesAll {
+    git branch -a
+}
+
+set-alias gba GitBranchesAll
